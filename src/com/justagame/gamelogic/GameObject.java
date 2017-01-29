2ca82ed0fc3cfa94631c3dbfe81dcac63e697e85
ricsi98 <98kissricsi@gmail.com>
2017-01-29 16:25:35 +0100

    GameObject
    draw meg translate meg update

--- src/com/justagame/gamelogic/GameObject.java
+++ src/com/justagame/gamelogic/GameObject.java
@@ -1,9 +1,13 @@
 package com.justagame.gamelogic;
 
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
 public class GameObject {
 	
 	public vec2 pos;
 	public vec2 vel;
+	public Rectangle collBox;
 	
 	public GameObject(){
 		
@@ -17,6 +21,11 @@ public class GameObject {
 		this.pos = pos;
 		this.vel = vel;
 	}
+	
+	public void setCollBox(int width, int height){
+		collBox = new Rectangle();
+		collBox.setBounds((int)pos.x, (int)pos.y, width, height);
+	}
 
 	public vec2 getPos() {
 		return pos;
@@ -24,6 +33,12 @@ public class GameObject {
 
 	public void setPos(vec2 pos) {
 		this.pos = pos;
+		collBox.setLocation((int)pos.x, (int)pos.y);
+	}
+	
+	public void translatePos(vec2 v){
+		pos.add(v);
+		collBox.setLocation((int)pos.x, (int)pos.y);
 	}
 
 	public vec2 getVel() {
@@ -34,5 +49,18 @@ public class GameObject {
 		this.vel = vel;
 	}
 	
+	public void update(float delta){
+		translatePos(vel);
+	}
 	
+	//EZT CSAK KIPROBALOM HOGY MUKODIK IGY
+	//gameObj.draw(g)
+	//ES AKKOR RAJZOL E A RENDERERBEN
+	public void draw(Graphics2D g, boolean drawCol){
+		g.drawRect((int)pos.x, (int)pos.y, 10,10);
+		if(drawCol){
+			g.drawRect((int)collBox.getX(), (int)collBox.getY(), 
+					(int)collBox.getWidth(), (int)collBox.getHeight());
+		}
+	}
 }
